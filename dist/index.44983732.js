5e01d47147b8fb39df5f5a15053c93d1bc8877af
johannahyll <johanna.hyll@gmail.com>
2025-03-01 20:59:27 +0100

    first commit

---
+++ dist/index.44983732.js
@@ -0,0 +1,8 @@
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+}
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+}
+
+//# sourceMappingURL=index.44983732.js.map
