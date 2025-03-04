d0e4a345937385525f6588c926d2b79ef265b1c0
johannahyll <johanna.hyll@gmail.com>
2025-03-04 12:26:59 +0100

    chart

--- dist/index.44983732.js
+++ dist/index.44983732.js
@@ -4,5 +4,33 @@ function openNav() {
 function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
 }
+const ctx = document.getElementById('myChart');
+new Chart(ctx, {
+    type: 'bar',
+    data: {
+        labels: [
+            'Red',
+            'Blue',
+            'Yellow',
+            'Green',
+            'Purple',
+            'Orange'
+        ],
+        datasets: [
+            {
+                label: '# of Votes',
+                data: [
+                    12,
+                    19,
+                    3,
+                    5,
+                    2,
+                    3
+                ],
+                borderWidth: 1
+            }
+        ]
+    }
+});
 
 //# sourceMappingURL=index.44983732.js.map
--- src/script.js
+++ src/script.js
@@ -6,3 +6,16 @@ function openNav() {
 function closeNav() {
   document.getElementById("mySidenav").style.width = "0";
 }
+
+const ctx = document.getElementById('myChart');
+new Chart(ctx, {
+type: 'bar',
+data: {
+labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+datasets: [{
+label: '# of Votes',
+data: [12, 19, 3, 5, 2, 3],
+borderWidth: 1
+}]
+},
+});
