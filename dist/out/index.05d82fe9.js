9c3954ba7b934f083f167a134a3c7fbe4d65ffc2
johannahyll <johanna.hyll@gmail.com>
2025-04-07 19:50:13 +0200

    rensning

--- dist/out/index.05d82fe9.js
+++
@@ -1,2 +0,0 @@
-(()=>{let e,t,l,s;let n=document.getElementsByClassName("prettyprint source linenums"),a=0,i=0;if(n&&n[0])for(s=document.location.hash.substring(1),l=(t=n[0].getElementsByTagName("li")).length;a<l;a++)i++,e=`line${i}`,t[a].id=e,e===s&&(t[a].className+=" selected")})();
-//# sourceMappingURL=index.05d82fe9.js.map
